cd66e997f89dd54886180e4e4a48b0c51e4c0e69
MI010 <getmasum97@gmail.com>
2025-08-09 15:41:06 +0530

    Aa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// IMPORTANT: replace with your exact repo name
 export default defineConfig({
   plugins: [react()],
-  base: "", // set to "/<repo>/" when deploying to GitHub Pages inside a repo
-})
+  base: "/portfolio-frontend/",
+});
