555954abfc2fdd4f5588d2b671760c3e68185f7e
MI010 <getmasum97@gmail.com>
2025-08-09 20:08:34 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// IMPORTANT: replace with your exact repo name
-export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio-frontend/",
-});
